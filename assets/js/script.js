ce8ebb04d97b3f163da722f2eadfd8403490ddfe
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-02-22 22:45:05 -0300

    Home att
    Home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
-/* Header transition */
+// Header transition
 window.addEventListener("load", () => {
-  let header = document.querySelector(".menu");
+  const header = document.querySelector(".menu");
   header.classList.add("fadeIn");
+
+  const homeTextLeft = document.querySelector(".home-animation-left");
+  homeTextLeft.classList.add("left-element");
+
+  const homeTextRight = document.querySelector(".home-animation-right");
+  homeTextRight.classList.add("right-element");
+});
+
+// Open CV
+document.querySelector("#pdf-button").addEventListener("click", () => {
+  const cvPath = "./assets/doc/Currículo Eros Netto Antunes.pdf";
+  window.open(cvPath, "_blank");
+});
+
+// Blur Hearder
+const header = document.querySelector(".menu");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 34) {
+    header.classList.add("menu-blur");
+  } else {
+    header.classList.remove("menu-blur");
+  }
 });
