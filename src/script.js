e53f9445b79095451434ab83d23e48f0d4338db3
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-02-23 19:18:12 -0300

    primeira versão do slider

--- src/script.js
+++ src/script.js
@@ -25,3 +25,37 @@ window.addEventListener("scroll", function () {
     header.classList.remove("menu-blur");
   }
 });
+
+// Slider
+const slider = document.querySelectorAll(".slider");
+const btnPrev = document.getElementById("prev-button");
+const btnNext = document.getElementById("next-button");
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach((item) => item.classList.remove("on"));
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add("on");
+}
+
+btnNext.addEventListener("click", () => {
+  hideSlider();
+  if (currentSlide === slider.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  showSlider();
+});
+btnPrev.addEventListener("click", () => {
+  hideSlider();
+  if (currentSlide === 0) {
+    currentSlide = slider.length - 1;
+  } else {
+    currentSlide--;
+  }
+  showSlider();
+});
